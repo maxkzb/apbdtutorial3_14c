c91c35a0b2e7a206901477080f3ac51e232dfa06
Maks Kozub <maks.kozub@gmail.com>
2025-03-19 12:04:27 +0100

    made the Container class make more sense

--- apbdtutorial3/Container.cs
+++ apbdtutorial3/Container.cs
@@ -6,9 +6,19 @@ public class Container
     public string SerialNumber { get; }
     public double MaxPayload { get; protected set; }
     public double TareWeight { get; protected set; }
+    public double CargoMass { get; private set; }
+    public double Height { get; set; }
+    public double Depth { get; set; }
+    public string ProductType { get; protected set; }
+    public double Temperature { get; protected set; }
     
-    protected Container(string type)
+    protected Container(string type, double tareWeight, double maxPayload, double height = 0, double depth = 0)
     {
         SerialNumber = $"KON-{type}-{_counter++}";
+        TareWeight = tareWeight;
+        MaxPayload = maxPayload;
+        Height = height;
+        Depth = depth;
+        CargoMass = 0;
     }
 }
