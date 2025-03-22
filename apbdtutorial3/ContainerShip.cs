4fc68fca6c93c782da16e3896c3809fec31b1d8c
Maks Kozub <maks.kozub@gmail.com>
2025-03-22 23:35:58 +0100

    new methods in container ship

--- apbdtutorial3/ContainerShip.cs
+++ apbdtutorial3/ContainerShip.cs
@@ -18,7 +18,7 @@ public class ContainerShip
     }
     public bool LoadContainer(Container container)
     {
-        if (Containers.Count >= MaxNumOfContainers || GetTotalWeight() + container.TareWeight + container.CargoMass > MaxWeight * 1000)
+        if (Containers.Count >= MaxNumOfContainers || GetTotalWeight() + (container.TareWeight + container.CargoMass) / 1000 > MaxWeight)
         {
             Console.WriteLine("Cannot load container. Ship capacity exceeded.");
             return false;
@@ -26,11 +26,39 @@ public class ContainerShip
         Containers.Add(container);
         return true;
     }
+    public void LoadContainersFromList(List<Container> containers)
+    {
+        foreach (var container in containers)
+        {
+            LoadContainer(container);
+        }
+    }
     public void RemoveContainer(Container container)
     {
         Containers.Remove(container);
     }
-
+    public void ReplaceContainer(string serialNumber, Container newContainer)
+    {
+        for (int i = 0; i < Containers.Count; i++)
+        {
+            if (Containers[i].SerialNumber == serialNumber)
+            {
+                Containers[i] = newContainer;
+                return;
+            }
+        }
+    }
+    public static void TransferContainer(ContainerShip fromShip, ContainerShip toShip, string serialNumber)
+    {
+        Container container = fromShip.Containers.Find(c => c.SerialNumber == serialNumber);
+        if (container != null)
+        {
+            if (toShip.LoadContainer(container))
+            {
+                fromShip.RemoveContainer(container);
+            }
+        }
+    }
     public double GetTotalWeight()
     {
         double totalWeight = 0;
@@ -43,6 +71,6 @@ public class ContainerShip
 
     public void PrintShipInfo()
     {
-        Console.WriteLine($"Ship: {Name}, Speed: {MaxSpeed} knots, Containers: {Containers.Count}/{MaxNumOfContainers}, Total Weight: {GetTotalWeight() / 1000} tons");
+        Console.WriteLine($"Ship: {Name}, Speed: {MaxSpeed} knots, Containers: {Containers.Count}/{MaxNumOfContainers}, Weight: {GetTotalWeight() / 1000}/{MaxWeight / 1000} tons");
     }
 }
--- apbdtutorial3/Program.cs
+++ apbdtutorial3/Program.cs
@@ -4,24 +4,50 @@ class Program
 {
     static void Main()
     {
-        RefrigeratedContainer bananaContainer = new RefrigeratedContainer(500, 2000, 250, 300, "Bananas", 5);
-        LiquidContainer milkContainer = new LiquidContainer(400, 1500, false);
-        GasContainer heliumContainer = new GasContainer(600, 1200, 30);
+        ContainerShip ship1 = new ContainerShip("Poseidon", 30, 5, 5);
+        ContainerShip ship2 = new ContainerShip("Neptune", 25, 5, 6);
         
-        bananaContainer.LoadCargo(1800);
-        milkContainer.LoadCargo(1300);
-        heliumContainer.LoadCargo(1000);
+        RefrigeratedContainer bananaContainer = new RefrigeratedContainer(300, 1000, 250, 200, "Bananas", 5);
+        LiquidContainer milkContainer = new LiquidContainer(250, 1200, false);
+        GasContainer heliumContainer = new GasContainer(200, 800, 50);
+        LiquidContainer fuelContainer = new LiquidContainer(270, 1100, true);
         
-        ContainerShip ship = new ContainerShip("Poseidon", 30, 3, 5000);
-        
-        ship.LoadContainer(bananaContainer);
-        ship.LoadContainer(milkContainer);
-        ship.LoadContainer(heliumContainer);
-        
-        ship.PrintShipInfo();
+        try
+        {
+            bananaContainer.LoadCargo(900);
+            milkContainer.LoadCargo(1000);
+            heliumContainer.LoadCargo(700);
+            fuelContainer.LoadCargo(500); // Should throw exception if exceeding 50% capacity
+        }
+        catch (OverfillException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
         
         bananaContainer.PrintCargo();
         milkContainer.PrintCargo();
         heliumContainer.PrintCargo();
+        fuelContainer.PrintCargo();
+        
+        ship1.LoadContainer(bananaContainer);
+        ship1.LoadContainer(milkContainer);
+        ship1.LoadContainer(heliumContainer);
+        ship1.LoadContainer(fuelContainer);
+        
+        ship1.PrintShipInfo();
+        
+        ship1.RemoveContainer(milkContainer);
+        Console.WriteLine("Milk container removed.");
+        ship1.PrintShipInfo();
+        
+        RefrigeratedContainer fishContainer = new RefrigeratedContainer(280, 900, 260, 220, "Fish", -2);
+        ship1.ReplaceContainer(bananaContainer.SerialNumber, fishContainer);
+        Console.WriteLine("Banana container replaced with fish container.");
+        ship1.PrintShipInfo();
+        
+        ContainerShip.TransferContainer(ship1, ship2, heliumContainer.SerialNumber);
+        Console.WriteLine("Helium container transferred to another ship.");
+        ship1.PrintShipInfo();
+        ship2.PrintShipInfo();
     }
 }
